83a295f24c4d674c4696a356d270f2b6913bcb5b
Van-Code <Van-Code@users.noreply.github.com>
2021-05-02 23:10:58 -0400

    triple sequence lesson 2

--- lessons_pt2.rb
+++ lessons_pt2.rb
@@ -190,3 +190,21 @@ def most_vowels(sentence)
     return count
   end
   print most_vowels("what a wonderful life") #=> "wonderful"
+
+
+  # Write a method triple_sequence that takes in two numbers, start and length. The method should return an array representing a sequence that begins with start and is length elements long. In the sequence, every element should be 3 times the previous element. Assume that the length is at least 1.
+
+  def triple_sequence(start, length)
+    new_arr = [start]
+    
+  while new_arr.length < length
+    new_arr << start * 3
+    start+=1
+  end
+    return new_arr
+  end
+  
+  print triple_sequence(2, 4) # => [2, 6, 18, 54]
+  puts
+  print triple_sequence(4, 5) # => [4, 12, 36, 108, 324]
+  puts
